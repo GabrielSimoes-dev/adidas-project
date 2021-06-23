0694c28a881f494406c7fa00e96dc51fecbf1681
GabrielSimoes-dev <84360387+GabrielSimoes-dev@users.noreply.github.com>
2021-06-23 18:43:57 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,66 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
+
+
+
+//Event Listeners
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck)
+filterOption.addEventListener('click', filterTodo);
+
+//Functions
+function addTodo(event){
+    //Prevent form from submitting
+    event.preventDefault();
+    //Todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //Check Mark Button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>'
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //Check trash button//
+    const  trashButton =  document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>'
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    //Append to list
+    todoList.appendChild(todoDiv);
+    //clear todo input value
+    todoInput.value = "";
+}
+
+
+function deleteCheck(e) {
+    const item = e.target;
+    //delete todo
+    if (item.classList[0] === 'trash-btn'){
+        const todo = item.parentElement;
+        //animaition
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend', function(){
+                todo.remove();
+        });
+       
+        
+    } 
+        //check mark
+        if (item.classList[0] === "complete-btn") {
+            const todo = item.parentElement;
+            todo.classList.toggle('completed');
+        }
+}
+
+function filterTodo(e){
+    const todos = todoList.hasChildNodes;
+    console.log(todos);
+}
